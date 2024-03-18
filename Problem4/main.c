e2d6792068d4de639632cd2e27b8b52e349f6f8b
khanhisme09 <khanhtlh.22itb@vku.udn.vn>
2024-03-18 18:09:53 +0700

    bai gk

--- Problem4/main.c
+++ Problem4/main.c
@@ -1,24 +1,26 @@
-#include <REGX51.H>
+#include <REGX52.H>
 
-void Timer0() interrupt 1{
-	
-	TH0 = 0xFB;
-	TL0 = 0xDC;
-	
-	P1 = 0x80;
+#define TIMER_PRESCALER 12  
+#define DUTY_CYCLE_PERCENTAGE 50  
+
+
+void delay(unsigned int count) {
+    unsigned int i;
+    for (i = 0; i < count; i++);
 }
-	
-void main(void){
-	
-	TMOD = 0x01;
-	
-	TH0 = 0xFB;
-	TL0 =0xDC;
-	
-	ET0 = 1;
-	EA = 1;
-	
-	TR0 = 1;
-	while(1){}
-	
+
+void main() {
+    TMOD = 0x01;  
+    
+    while(1) {
+        TH0 = 0xFE;   
+        TL0 = 0x0C;   
+
+        P1 |= (1 << 7);
+
+        delay((DUTY_CYCLE_PERCENTAGE * TIMER_PRESCALER) / 100);
+
+        P1 &= ~(1 << 7);
+        delay(((100 - DUTY_CYCLE_PERCENTAGE) * TIMER_PRESCALER) / 100);
+    }
 }
